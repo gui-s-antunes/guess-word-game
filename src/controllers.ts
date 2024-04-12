856a1d8f52525cb53a6a6631e6819ae8d7695d89
Guilherme Antunes <guilherme.silva.antunes1999@gmail.com>
2024-04-12 15:42:04 -0300

    added functions that check if a letter or an valid button is pressed

--- src/controllers.ts
+++ src/controllers.ts
@@ -8,7 +8,17 @@ import { selectWords } from './utils/selectWords';
 export const game = document.querySelector('.game') as HTMLDivElement;
 export const word = document.querySelector('.word') as HTMLTableElement;
 
-generateWordBlocks(word, 6, 5);
+generateWordBlocks(word);
+
+interface GameActions {
+  [key: string]: () => void;
+}
+
+const gameActions: GameActions = {
+  ArrowRight: blockToRight,
+  ArrowLeft: blocktoLeft,
+  Enter: submitWord,
+};
 
 // export interface Palavra {
 //   [key: string]: number;
@@ -22,6 +32,44 @@ console.log('palavras do data: ', wordsFromData);
 
 const words = new Words(wordsFromData);
 
-const selectedWords = selectWords(words.words, 5);
+const selectedWords = selectWords(words.words); //(words, numofwords)
 
 console.log('Palavras escolhidas: ', selectedWords);
+
+document.addEventListener('keydown', (event) => {
+  console.log('event key: ', event.key);
+  if (event.key.length === 1) {
+    if (isPressedKeyALetter(event.key)) {
+      addLetterToBlock();
+      return;
+    }
+  }
+
+  gameAction(event.key);
+});
+
+function isPressedKeyALetter(key: string) {
+  return key.match(/[a-z]/i);
+}
+
+function gameAction(key: string) {
+  const fn = gameActions[key];
+  console.log('fn value: ', fn);
+  if (fn) fn();
+}
+
+function addLetterToBlock() {
+  console.log('add letter to block');
+}
+
+function submitWord() {
+  console.log('submit this word try (word validator needed)');
+}
+
+function blocktoLeft() {
+  console.log('move block used to left');
+}
+
+function blockToRight() {
+  console.log('move block used to right');
+}
