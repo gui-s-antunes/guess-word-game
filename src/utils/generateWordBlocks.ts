6ed666f4bddae3df02685fb5fdac3c25e75f864e
Guilherme Antunes <guilherme.silva.antunes1999@gmail.com>
2024-04-12 11:33:14 -0300

    num of blocks is optional and the first block is set as the selected block

--- src/utils/generateWordBlocks.ts
+++ src/utils/generateWordBlocks.ts
@@ -1,16 +1,20 @@
 export function generateWordBlocks(
   table: HTMLTableElement,
-  rows: number,
-  cells: number,
+  rows?: number,
+  cells?: number,
 ): void {
-  for (let i = 0; i < rows; i++) {
+  for (let i = 0; i < (rows || 6); i++) {
     const tr = table.insertRow();
 
-    for (let j = 0; j < cells; j++) {
+    for (let j = 0; j < (cells || 5); j++) {
       const td = tr.insertCell();
       const text = document.createTextNode(`Cell I${i}/J${j}`); // texto a ser inserido na célula
       td.appendChild(text); // texto na célula inserido
       td.style.border = '1px solid black';
+
+      if (j === 0 && i === 0) {
+        td.setAttribute('class', 'selectedPosition');
+      }
     }
   }
 }
