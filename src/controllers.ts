aa52fe3fb9cf7c0fafd81d913232c1dfa68c81ae
Guilherme Antunes <guilherme.silva.antunes1999@gmail.com>
2024-04-15 16:30:48 -0300

    functions which move the controlled block to left or right are done

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,14 +1,14 @@
 console.log('Controllers.ts is here!');
 
-import { Words } from './game';
+import { Words, Game } from './game';
 import { generateWordBlocks } from './utils/generateWordBlocks';
 import { getWords } from './utils/getWords';
 import { selectWords } from './utils/selectWords';
 
-export const game = document.querySelector('.game') as HTMLDivElement;
-export const word = document.querySelector('.word') as HTMLTableElement;
+export const $game = document.querySelector('.game') as HTMLDivElement;
+export const $word = document.querySelector('.word') as HTMLTableElement;
 
-generateWordBlocks(word);
+generateWordBlocks($word);
 
 interface GameActions {
   [key: string]: () => void;
@@ -33,6 +33,8 @@ console.log('palavras do data: ', wordsFromData);
 const words = new Words(wordsFromData);
 
 const selectedWords = selectWords(words.words); //(words, numofwords)
+
+const game = new Game(selectedWords);
 
 console.log('Palavras escolhidas: ', selectedWords);
 
@@ -68,8 +70,30 @@ function submitWord() {
 
 function blocktoLeft() {
   console.log('move block used to left');
+
+  const currentCellPosition: number = game.cellPosition;
+  const rows = $word.getElementsByTagName('tr');
+  console.log('number of rows: ', rows.length);
+
+  const cells = rows[game.rowPosition].getElementsByTagName('td');
+
+  if (currentCellPosition > 0) {
+    cells[currentCellPosition].removeAttribute('class');
+    cells[currentCellPosition - 1].setAttribute('class', 'selectedPosition');
+    game.cellPosition = currentCellPosition - 1;
+  }
 }
 
 function blockToRight() {
   console.log('move block used to right');
+
+  const currentCellPosition: number = game.cellPosition;
+  const rows = $word.getElementsByTagName('tr');
+  const cells = rows[game.rowPosition].getElementsByTagName('td');
+
+  if (currentCellPosition < cells.length - 1) {
+    cells[currentCellPosition].removeAttribute('class');
+    cells[currentCellPosition + 1].setAttribute('class', 'selectedPosition');
+    game.cellPosition = currentCellPosition + 1;
+  }
 }
